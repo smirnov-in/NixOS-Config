ac00a100b6f26ebf0d70c8fb527e30c40858d1a3
smirnov-in <71039513+smirnov-in@users.noreply.github.com>
2025-06-25 08:58:29 +0300

    Add openconnect to owlery

--- home/duck/owlery/default.nix
+++ home/duck/owlery/default.nix
@@ -7,6 +7,7 @@
     ../common/optional/libreoffice.nix
     ../common/optional/mpv.nix
     ../common/optional/firefox.nix
+    ../common/optional/openconnect.nix
     ../common/optional/telegram.nix
     ../common/optional/vscode.nix
   ];
