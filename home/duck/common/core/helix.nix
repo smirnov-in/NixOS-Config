d5f4fae199e2c23d622a76a5d496c8a56851a2ad
smirnov-in <71039513+smirnov-in@users.noreply.github.com>
2025-07-08 18:05:18 +0300

    Change helix config

--- home/duck/common/core/helix.nix
+++ home/duck/common/core/helix.nix
@@ -12,56 +12,15 @@
           cursor-line = "error";
         };
       };
-
-      keys = {
-        insert = {
-          j.j = "normal_mode";
-        };
-      };
     };
 
-    languages = {
-      language-server = {
-        nixd = {
-          command = "nixd";
-        };
-
-        coq-lsp = {
-          command = "coq-lsp";
-        };
-      };
-
-      language = [
-        {
-          name = "nix";
-          scope = "source.nix";
-          injection-regex = "nix";
-          file-types = ["nix"];
-          comment-token = "#";
-          formatter.command = "alejandra";
-          auto-format = true;
-          indent = {
-            tab-width = 2;
-            unit = "  ";
-          };
-          language-servers = ["nixd"];
-        }
-        {
-          name = "coq";
-          scope = "source.v";
-          file-types = ["v"];
-          block-comment-tokens = {
-            start = "(*";
-            end = "*)";
-          };
-          indent = {
-            tab-width = 2;
-            unit = "  ";
-          };
-          language-servers = ["coq-lsp"];
-        }
-      ];
-    };
+    languages.language = [
+      {
+        name = "nix";
+        formatter.command = "alejandra";
+        auto-format = true;
+      }
+    ];
   };
 
   home.packages = with pkgs; [
