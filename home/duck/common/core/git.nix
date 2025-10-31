a96301b96206e6c9bd767d61f96c90a75756fe58
smirnov-in <71039513+smirnov-in@users.noreply.github.com>
2025-10-31 13:10:56 +0300

    Fix git config

--- home/duck/common/core/git.nix
+++ home/duck/common/core/git.nix
@@ -2,7 +2,9 @@
   programs.git = {
     enable = true;
     
-    userName = "smirnov-in";
-    userEmail = "71039513+smirnov-in@users.noreply.github.com";
+    settings.user = {
+      name = "smirnov-in";
+      email = "71039513+smirnov-in@users.noreply.github.com";
+    };
   };
 }
