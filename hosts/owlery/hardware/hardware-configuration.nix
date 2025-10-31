b2e4d0d9b770f80d2b7ee6c12a8439b82f158fe4
smirnov-in <71039513+smirnov-in@users.noreply.github.com>
2025-10-31 13:07:29 +0300

    Add support for razer mouse

--- hosts/owlery/hardware/hardware-configuration.nix
+++ hosts/owlery/hardware/hardware-configuration.nix
@@ -1,27 +1,31 @@
-{ config, lib, pkgs, modulesPath, ... }:
-
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
-  
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
   boot = {
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    
+
     kernelPackages = pkgs.linuxPackages_latest;
-    kernelModules = [ "kvm-intel" ];
-    extraModulePackages = [ ];
-    kernelParams = [ "quiet" "udev.log_level=3" ];
+    kernelModules = ["kvm-intel"];
+    extraModulePackages = [];
+    kernelParams = ["quiet" "udev.log_level=3"];
     consoleLogLevel = 0;
     plymouth.enable = true;
-    
+
     initrd = {
       verbose = false;
-      availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
-      kernelModules = [ "dm-snapshot" ];
+      availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod"];
+      kernelModules = ["dm-snapshot"];
     };
   };
 
@@ -30,7 +34,12 @@
     networkmanager.enable = true;
     useDHCP = lib.mkDefault true;
   };
-  
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.openrazer = {
+    enable = true;
+    users = ["duck"];
+  };
 }
