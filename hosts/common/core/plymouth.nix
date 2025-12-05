c40d96afd6fa80b85fae1ac56e3ab7a8bdeaaf66
smirnov-in <71039513+smirnov-in@users.noreply.github.com>
2025-12-05 17:17:50 +0300

    Fix plymouth resolution

--- hosts/common/core/plymouth.nix
+++ hosts/common/core/plymouth.nix
@@ -1,6 +1,9 @@
 {
   boot = {
-    plymouth.enable = true;
+    plymouth = {
+      enable = true;
+      extraConfig = "DeviceScale=1";
+    };
     loader.timeout = 0;
 
     kernelParams = ["quiet" "splash" "boot.shell_on_fail" "udev.log_priority=3" "rd.systemd.show_status=auto"];
