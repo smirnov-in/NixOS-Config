0a488088cea5f4874281cdc4444b64018a78f6bb
smirnov-in <71039513+smirnov-in@users.noreply.github.com>
2025-12-05 15:04:25 +0300

    Niri config

--- home/duck/common/optional/niri.nix
+++ home/duck/common/optional/niri.nix
@@ -19,13 +19,27 @@ in {
         layout = "us,ru";
         options = "shift:both_capslock_cancel,caps:escape,grp:win_space_toggle";
       };
+
+      trackpoint = {
+        accel-profile = "flat";
+        accel-speed = 0.2;
+      };
     };
+
+    gestures = {
+      hot-corners.enable = false;
     };
+
     prefer-no-csd = true;
     hotkey-overlay.skip-at-startup = true;
 
     layout = {
       shadow.enable = true;
+      gaps = 12;
+      focus-ring = {
+        enable = true;
+        active.color = "#cba6f7";
+      };
     };
 
     window-rules = [
@@ -62,16 +76,38 @@ in {
       "Mod+J".action = focus-workspace-down;
       "Mod+K".action = focus-workspace-up;
       "Mod+L".action = focus-column-right;
+      "Mod+U".action = focus-window-down;
+      "Mod+I".action = focus-window-up;
 
       "Mod+Shift+H".action = move-column-left;
       "Mod+Shift+J".action = move-column-to-workspace-down;
       "Mod+Shift+K".action = move-column-to-workspace-up;
       "Mod+Shift+L".action = move-column-right;
+      "Mod+Shift+U".action = move-window-down;
+      "Mod+Shift+I".action = move-window-up;
+
+      "Mod+BracketLeft".action = consume-or-expel-window-left;
+      "Mod+BracketRight".action = consume-or-expel-window-right;
+      "Mod+Comma".action = consume-window-into-column;
+      "Mod+Period".action = expel-window-from-column;
 
       "Mod+O".action = toggle-overview;
 
+      "Mod+R".action = switch-preset-column-width;
+      "Mod+Shift+R".action = switch-preset-window-height;
+      "Mod+Ctrl+R".action = reset-window-height;
       "Mod+F".action = maximize-column;
       "Mod+Shift+F".action = fullscreen-window;
+      "Mod+Ctrl+F".action = expand-column-to-available-width;
+
+      "Mod+C".action = center-column;
+      "Mod+Shift+C".action = center-visible-columns;
+
+      "Mod+Minus".action = set-column-width "-10%";
+      "Mod+Equal".action = set-column-width "+10%";
+      "Mod+Shift+Minus".action = set-window-height "-10%";
+      "Mod+Shift+Equal".action = set-window-height "+10%";
+
       "Mod+G".action = toggle-window-floating;
       "Mod+Shift+G".action = switch-focus-between-floating-and-tiling;
       "Mod+Q".action = close-window;
@@ -85,6 +121,10 @@ in {
       "XF86AudioMute".action.spawn = noctalia "volume muteOutput";
       "XF86MonBrightnessDown".action.spawn = noctalia "brightness decrease";
       "XF86MonBrightnessUp".action.spawn = noctalia "brightness increase";
+
+      "Print".action.screenshot = {};
+      "Mod+Print".action.screenshot-screen = {};
+      "Shift+Print".action.screenshot-window = {};
     };
   };
 
