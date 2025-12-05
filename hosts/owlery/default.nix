b03347bb1ba77d03feb659b165e12314614ea4bd
smirnov-in <71039513+smirnov-in@users.noreply.github.com>
2025-12-05 14:43:38 +0300

    Enable niri, steam and transmission for owlery

--- hosts/owlery/default.nix
+++ hosts/owlery/default.nix
@@ -5,6 +5,9 @@
     ../common/core
 
     ../common/optional/fingerprint.nix
+    ../common/optional/niri.nix
+    ../common/optional/steam.nix
+    ../common/optional/transmission.nix
 
     ../common/users/duck
   ];
