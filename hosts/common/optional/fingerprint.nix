c6575c363759251806974be31d3cbe771ec543a5
smirnov-in <71039513+smirnov-in@users.noreply.github.com>
2025-12-31 13:30:38 +0300

    Disable fingerprint when lid is closed

--- hosts/common/optional/fingerprint.nix
+++ hosts/common/optional/fingerprint.nix
@@ -1,5 +1,21 @@
 {
-  security.pam = {
-    services.sudo.fprintAuth = true;
+  config,
+  pkgs,
+  ...
+}: {
+  security.pam.services.sudo.fprintAuth = true;
+  security.pam.services.sudo.rules.auth = {
+    fprintd-only-if-lid-open = {
+      enable = true;
+      order = config.security.pam.services.sudo.rules.auth.fprintd.order - 1;
+      control = "[success=ok default=1]";
+      modulePath = "${config.security.pam.package}/lib/security/pam_exec.so";
+      args = [
+        "quiet"
+        "${pkgs.writeShellScript "is-lid-open" ''
+          ${pkgs.gnugrep}/bin/grep -q "^state:.*open" "/proc/acpi/button/lid/LID/state"
+        ''}"
+      ];
+    };
   };
 }
