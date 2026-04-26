4139ab300ba12fbbf1a999d5e6287040be8b805b
smirnov-in <71039513+smirnov-in@users.noreply.github.com>
2026-04-26 12:26:19 +0300

    Change transmission download dir

--- hosts/common/optional/transmission.nix
+++ hosts/common/optional/transmission.nix
@@ -2,5 +2,19 @@
   services.transmission = {
     enable = true;
     package = pkgs.transmission_4;
+    user = "duck";
+
+    settings = {
+      "download-dir" = "/home/duck/Downloads/transmission";
+      "incomplete-dir" = "/home/duck/Downloads/transmission/.incomplete";
+      "incomplete-dir-enabled" = true;
+    };
   };
+
+  system.activationScripts.transmissionDirs.text = ''
+    mkdir -p /home/duck/Downloads/transmission/.incomplete
+    chown -R duck:transmission /home/duck/Downloads/transmission
+    chmod 750 /home/duck/Downloads/transmission
+    chmod 750 /home/duck/Downloads/transmission/.incomplete
+  '';
 }
