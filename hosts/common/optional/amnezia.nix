4230aa5a304b7c11587ca1c61241054b8a758848
smirnov-in <71039513+smirnov-in@users.noreply.github.com>
2026-04-27 21:02:25 +0300

    Replace AmneziaVPN client with awg-tools

--- hosts/common/optional/amnezia.nix
+++ hosts/common/optional/amnezia.nix
@@ -1,3 +1,12 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
+  boot = {
+    extraModulePackages = [config.boot.kernelPackages.amneziawg];
+    kernelModules = ["amneziawg"];
+  };
+
   environment.systemPackages = [pkgs.amneziawg-tools];
 }
