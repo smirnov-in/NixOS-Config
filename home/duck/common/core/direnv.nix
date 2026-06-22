8f2a02018364069cd3574fb043e273b8b9ffcfb1
smirnov-in <71039513+smirnov-in@users.noreply.github.com>
2026-06-22 12:23:08 +0300

    Make direnv silent

--- home/duck/common/core/direnv.nix
+++ home/duck/common/core/direnv.nix
@@ -2,10 +2,12 @@
   lib,
   options,
   ...
-}: {
+}:
+{
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    silent = true;
     config = {
       hide_env_diff = true;
     };
