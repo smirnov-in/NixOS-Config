bf05c7f5e0975c2af39d00962e288659b21769ae
smirnov-in <71039513+smirnov-in@users.noreply.github.com>
2026-07-07 16:19:02 +0300

    Define Nest storage layout

--- hosts/nest/storage.nix
+++ hosts/nest/storage.nix
@@ -1,10 +1,25 @@
+let
+  mediaDirs = [
+    "/srv/media"
+    "/srv/media/movies"
+    "/srv/media/music"
+    "/srv/media/tv"
+  ];
+  downloadDirs = [
+    "/srv/downloads"
+    "/srv/downloads/complete"
+    "/srv/downloads/incomplete"
+    "/srv/downloads/watch"
+  ];
+in
 {
   users.groups.media = { };
 
   users.users.duck.extraGroups = [ "media" ];
 
   systemd.tmpfiles.rules = [
-    "d /srv/media 2775 root media - -"
+    "d /srv 0755 root root - -"
     "d /srv/backups 0750 root root - -"
-  ];
+  ]
+  ++ map (dir: "d ${dir} 2775 root media - -") (mediaDirs ++ downloadDirs);
 }
