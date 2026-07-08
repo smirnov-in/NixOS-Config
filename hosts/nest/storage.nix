fd69eb09ab928062fc84f4f604366a28382e7e84
smirnov-in <71039513+smirnov-in@users.noreply.github.com>
2026-07-08 16:55:15 +0300

    Avoid duplicate tmpfiles rule for srv

--- hosts/nest/storage.nix
+++ hosts/nest/storage.nix
@@ -18,7 +18,6 @@ in
   users.users.duck.extraGroups = [ "media" ];
 
   systemd.tmpfiles.rules = [
-    "d /srv 0755 root root - -"
     "d /srv/backups 0750 root root - -"
   ]
   ++ map (dir: "d ${dir} 2775 root media - -") (mediaDirs ++ downloadDirs);
